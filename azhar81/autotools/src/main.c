726610853441bad70da8c416d62e7aa1f359c04a
Muhammad Azhar Hassanuddin <a.hassanuddin02@gmail.com>
2022-03-01 13:40:25 +0700

    autotools: redone

--- azhar81/autotools/src/main.c
+++ azhar81/autotools/src/main.c
@@ -1,10 +1,13 @@
 #include <config.h>
 #include <stdio.h>
+//  ***  CHANGE THIS! **************
+#define  MYNAME "azhar81"
+
 int
 main (void)
 {
-    puts ("Hello World!");
-    puts ("I am azhar81 and this is " PACKAGE_STRING ".");
-    return 0;
+  puts ("Hello World!");
+  puts ("I am " MYNAME ".");
+  puts ("This is " PACKAGE_STRING ".");
+  return 0;
 }
-
