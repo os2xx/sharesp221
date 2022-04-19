0a771784ba76d016729c97e6390e86f94fb7a710
sunnysani <naufal.sani2001@gmail.com>
2022-04-19 14:44:11 +0700

    hello01.c

---
+++ sunnysani/QUIZ07/hello01.c
@@ -0,0 +1,14 @@
+#define ZCZCHEADER  "ZCZC M01"
+#define DESCRIPTION "hello01"
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void) {
+    pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "START");
+    return 0;
+}
+
+void cleanup_module(void) {
+    pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "STOP");
+}
