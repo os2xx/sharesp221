e8116026cb52efe59c239f2088fba0da5db2a9b3
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2022-04-19 14:42:39 +0700

    Q07 M02

---
+++ faishol01/QUIZ07/hello02b.c
@@ -0,0 +1,12 @@
+/*
+ * hello-1.c - The simplest kernel module.
+ */
+#include <linux/kernel.h> /* Needed for pr_info() */
+#include <linux/module.h> /* Needed by all modules */
+
+void cleanup_module(void)
+{
+    pr_info("ZCZC M02 hello02 STOP\n");
+}
+
+MODULE_LICENSE("GPL");
