a009cc7ea33d344aba8c62f3f87f2a0f891aca8d
Martin Samuel Sjarief <samuel92@ui.ac.id>
2022-04-19 14:53:21 +0700

    Finished module hello02

--- kalEl2001/QUIZ07/hello01.c
+++ kalEl2001/QUIZ07/hello01.c
@@ -1,20 +1,26 @@
-/*
- * hello-1.c - The simplest kernel module.
- */
-#include <linux/kernel.h> /* Needed for pr_info() */
-#include <linux/module.h> /* Needed by all modules */
+#define VERSION     "REV00"
+// REV03: Mon 18 Apr 2022 22:00:00 WIB
+// REV02: Sun 03 Apr 2022 14:30:00 WIB
+// START: Wed 30 Mar 2022 16:30:00 WIB
+// (c) 2022-2022 It is a FREE GSGS one.
+#define ZCZCHEADER  "ZCZC M01"
+#define DESCRIPTION "hello01"
+#define AUTHOR      "Samuel"
+#define LICENSE     "GPL"
 
-int init_module(void)
-{
-    pr_info("Hello world 1.\n");
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-    /* A non 0 return means init_module failed; module can't be loaded. */
+int init_module(void) {
+    pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "START");
     return 0;
 }
 
-void cleanup_module(void)
-{
-    pr_info("Goodbye world 1.\n");
+void cleanup_module(void) {
+    pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "STOP");
 }
 
-MODULE_LICENSE("GPL");
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESCRIPTION);
+MODULE_LICENSE(LICENSE);
+MODULE_VERSION(VERSION);
