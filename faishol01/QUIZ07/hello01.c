c2c4975f74408ea9e73ec3d881779ddd2ef82a1a
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2022-04-19 14:18:38 +0700

    M01

---
+++ faishol01/QUIZ07/hello01.c
@@ -0,0 +1,20 @@
+/*
+ * hello-1.c - The simplest kernel module.
+ */
+#include <linux/kernel.h> /* Needed for pr_info() */
+#include <linux/module.h> /* Needed by all modules */
+
+int init_module(void)
+{
+    pr_info("ZCZC M01 hello01 START\n");
+
+    /* A non 0 return means init_module failed; module can't be loaded. */
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    pr_info("ZCZC M01 hello01 STOP\n");
+}
+
+MODULE_LICENSE("GPL");
