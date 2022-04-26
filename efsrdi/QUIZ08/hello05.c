5fc3495b8a75c45867e8a4f807ae794fd32671b6
Efrado Suryadi <58604575+efsrdi@users.noreply.github.com>
2022-04-26 14:21:52 +0700

    Finish Q08

---
+++ efsrdi/QUIZ08/hello05.c
@@ -0,0 +1,19 @@
+#define ZCZCHEADER  "ZCZC M05"
+#define DESCRIPTION "hello05"
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init hello_5_init(void) {
+	pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "START");
+	return 0;
+}
+
+static void __exit hello_5_exit(void) {
+        pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "STOP");
+}
+
+module_init(hello_5_init);
+module_exit(hello_5_exit);
+MODULE_LICENSE("GPL");
