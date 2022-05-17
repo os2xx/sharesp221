a63d4c94ee00568e683ccfbd8a16604f0f0771e1
Martin Samuel Sjarief <samuel92@ui.ac.id>
2022-05-17 13:29:59 +0700

    kalEl2001: copy week 9 to week 10 and fix module 3

---
+++ kalEl2001/QUIZ10/hello05.c
@@ -0,0 +1,28 @@
+#include <linux/init.h> /* Needed for the macros */
+#include <linux/kernel.h> /* Needed for pr_info() */
+#include <linux/module.h> /* Needed by all modules */
+
+#define ZCZCHEADER  "ZCZC M05"
+#define DESCRIPTION "hello05"
+#define AUTHOR      "Samuel"
+#define LICENSE     "GPL"
+#define VERSION     "REV00"
+
+static int __init hello05_init(void)
+{
+    pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "START");
+    return 0;
+}
+
+static void __exit hello05_exit(void)
+{
+    pr_info("%s %s %s\n", ZCZCHEADER, DESCRIPTION, "STOP");
+}
+
+module_init(hello05_init);
+module_exit(hello05_exit);
+
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESCRIPTION);
+MODULE_LICENSE(LICENSE);
+MODULE_VERSION(VERSION);
