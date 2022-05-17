f7da2ad14ad78c75894c7e38e57cdb5ef36be1da
aldariaski <70104051+aldariaski@users.noreply.github.com>
2022-05-17 14:38:14 +0700

    aldariaski w10

---
+++ aldariaski/QUIZ10/mkfs-simplefs.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+
+#include "simple.h"
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    ssize_t ret;
+    struct simplefs_super_block sb;
+
+    if (argc != 2) {
+        printf("Usage: mkfs-simplefs <device>\n");
+        return -1;
+    }
+
+    fd = open(argv[1], O_RDWR);
+    if (fd == -1) {
+        perror("Error opening the device");
+        return -1;
+    }
+
+    sb.version = 1;
+    sb.magic = SIMPLEFS_MAGIC;
+    sb.block_size = SIMPLEFS_DEFAULT_BLOCK_SIZE;
+    sb.free_blocks = ~0;
+
+    sb.root_inode.mode = S_IFDIR;
+    sb.root_inode.inode_no = SIMPLEFS_ROOT_INODE_NUMBER;
+    sb.root_inode.data_block_number = SIMPLEFS_ROOTDIR_DATABLOCK_NUMBER;
+    sb.root_inode.dir_children_count = 0;
+
+    ret = write(fd, (char *)&sb, sizeof(sb));
+
+    /* Just a redundant check. Not required ideally. */
+    if (ret != SIMPLEFS_DEFAULT_BLOCK_SIZE)
+        printf
+            ("bytes written [%d] are not equal to the default block size\n",
+             (int)ret);
+    else
+        printf("Super block written succesfully\n");
+
+    close(fd);
+
+    return 0;
+}
---
+++ aldariaski/QUIZ10/simple.h
@@ -0,0 +1,33 @@
+#define SIMPLEFS_MAGIC 0x10032013
+#define SIMPLEFS_DEFAULT_BLOCK_SIZE 4096
+#define SIMPLEFS_FILENAME_MAXLEN 255
+
+const int SIMPLEFS_ROOT_INODE_NUMBER = 1;
+const int SIMPLEFS_ROOTDIR_DATABLOCK_NUMBER = 2;
+
+struct simplefs_dir_record {
+    char filename[SIMPLEFS_FILENAME_MAXLEN];
+    uint32_t inode_no;
+};
+
+struct simplefs_inode {
+    mode_t mode;
+    uint32_t inode_no;
+    uint32_t data_block_number;
+
+    union {
+        uint32_t file_size;
+        uint32_t dir_children_count;
+    };
+};
+
+struct simplefs_super_block {
+    uint32_t version;
+    uint32_t magic;
+    uint32_t block_size;
+    uint32_t free_blocks;
+
+    struct simplefs_inode root_inode;
+
+    char padding[SIMPLEFS_DEFAULT_BLOCK_SIZE - (4 * sizeof(uint32_t)) - sizeof(struct simplefs_inode)];
+};
