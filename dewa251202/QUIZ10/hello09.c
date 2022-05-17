592acdfb5646d128b8eee842f361ed4815993a3d
Dewangga Putra Sheradhien <dewa251202@gmail.com>
2022-05-17 13:37:20 +0700

    Update Makefile and hello09.c

--- dewa251202/QUIZ10/hello09.c
+++ dewa251202/QUIZ10/hello09.c
@@ -1,23 +1,23 @@
 #include <linux/kernel.h> /* Needed for pr_info() */ 
 #include <linux/module.h> /* Needed by all modules */ 
 
-static int __init init_hello05(void) 
+static int __init init_hello09(void) 
 { 
-    pr_info("ZCZC M05 hello05 START\n"); 
+    pr_info("ZCZC M09 hello09 START\n"); 
 
     /* A non 0 return means init_module failed; module can't be loaded. */ 
     return 0; 
 } 
 
-static void __exit exit_hello05(void) 
+static void __exit exit_hello09(void) 
 { 
-    pr_info("ZCZC M05 hello05 STOP\n"); 
+    pr_info("ZCZC M09 hello09 STOP\n"); 
 } 
 
-module_init(init_hello05);
-module_exit(exit_hello05);
+module_init(init_hello09);
+module_exit(exit_hello09);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("LKMPG team, modified by dewa251202");
+MODULE_AUTHOR("GSGS & dewa251202");
 MODULE_VERSION("REV02");
-MODULE_DESCRIPTION("hello05");
+MODULE_DESCRIPTION("A module for simplefs");
