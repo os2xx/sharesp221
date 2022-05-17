b9a83bc34c9543f0d38ef4bbe488de68c630be49
WhReen <54948253+WhReen@users.noreply.github.com>
2022-05-17 14:16:44 +0700

    sixth progress, referenceing LinuxCourses's simplefs (halfway)

--- WhReen/QUIZ10/hello09.c
+++ WhReen/QUIZ10/hello09.c
@@ -14,6 +14,8 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 
+#include "simple.h"
+
 static int simplefs_readdir(struct file *filp, void *dirent, filldir_t filldir)
 {
     /* ls will list nothing as of now.
@@ -77,7 +79,7 @@ int simplefs_fill_super(struct super_block *sb, void *data, int silent)
     struct inode *inode;
 
     /* A magic number that uniquely identifies our filesystem type */
-    sb->s_magic = 0x10032013;
+    sb->s_magic = SIMPLEFS_MAGIC;
 
     inode = simplefs_get_inode(sb, NULL, S_IFDIR, 0);
     inode->i_op = &simplefs_inode_ops;
