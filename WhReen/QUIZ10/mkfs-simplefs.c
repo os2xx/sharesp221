f4ef3af7a7f4ba0586801f727277e35b29d224fc
WhReen <54948253+WhReen@users.noreply.github.com>
2022-05-17 14:30:04 +0700

    seventh progress, referenceing LinuxCourses's simplefs (halfway)

--- WhReen/QUIZ10/mkfs-simplefs.c
+++ WhReen/QUIZ10/mkfs-simplefs.c
@@ -1,10 +1,11 @@
-#include "simple.h"
-
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdint.h>
+
+#include "simple.h"
 
 int main(int argc, char *argv[])
 {
@@ -28,15 +29,20 @@ int main(int argc, char *argv[])
     sb.block_size = SIMPLEFS_DEFAULT_BLOCK_SIZE;
     sb.free_blocks = ~0;
 
+    sb.root_inode.mode = S_IFDIR;
+    sb.root_inode.inode_no = SIMPLEFS_ROOT_INODE_NUMBER;
+    sb.root_inode.data_block_number = SIMPLEFS_ROOTDIR_DATABLOCK_NUMBER;
+    sb.root_inode.dir_children_count = 0;
+
     ret = write(fd, (char *)&sb, sizeof(sb));
 
     /* Just a redundant check. Not required ideally. */
     if (ret != SIMPLEFS_DEFAULT_BLOCK_SIZE)
         printf
-            ("bytes written [%d] are not equal to the default block size",
+            ("bytes written [%d] are not equal to the default block size\n",
              (int)ret);
     else
-        printf("Super block written succesfully");
+        printf("Super block written succesfully\n");
 
     close(fd);
 
