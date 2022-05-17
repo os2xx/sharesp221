1f76fd6471b24a2c1eb76ee19002f7bdaec79d20
sunnysani <naufal.sani2001@gmail.com>
2022-05-17 13:29:49 +0700

    Quiz09 to Quiz10 - a

---
+++ sunnysani/QUIZ10/hello02a.c
@@ -0,0 +1,22 @@
+#define VERSION     "v1.1"
+#define ZCZCHEADER  "ZCZC M02"
+#define DESCRIPTION "hello02"
+#define AUTHOR      "Naufal Sani"
+#define LICENSE     "GPL"
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init hello02_init(void)
+{
+    pr_info("%s %s%s\n", ZCZCHEADER, DESCRIPTION, ": A module with multiple files - START");
+    return 0;
+}
+
+module_init(hello02_init);
+
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESCRIPTION);
+MODULE_LICENSE(LICENSE);
+MODULE_VERSION(VERSION);
